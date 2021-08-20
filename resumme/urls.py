fc778bea0eb0f450412549a22bfe2c10f04308ef
Anyijukire <anyijukirjanett@gmail.com>
2021-08-20 18:33:02 +0300

    last resume

--- resumme/urls.py
+++ resumme/urls.py
@@ -3,7 +3,7 @@ from .views import contact, show_messages, show_resume
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns= [
-    path('contact/', contact, name="contact"),
-    path('resume/' , show_resume, name='show_resume'),
+    path('/' , show_resume, name='show_resume'),
+    path('contact/', contact, name="contact"),    
     path('messages/', show_messages, name='show_messages')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
